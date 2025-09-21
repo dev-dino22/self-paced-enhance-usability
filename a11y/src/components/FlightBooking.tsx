57138017cc41fdefca4068fd5a39752e26f63244
dev-dino22 <wlsskdud02@naver.com>
2025-09-21 18:26:14 +0900

    reefactor: 승객 수 최소/최대 값 도달 시 메세지 알림 기능 추가

--- a11y/src/components/FlightBooking.tsx
+++ a11y/src/components/FlightBooking.tsx
@@ -2,16 +2,30 @@ import { useState } from "react";
 
 import "./FlightBooking.css";
 
+const MIN_PASSENGERS = 1;
 const MAX_PASSENGERS = 3;
 
 const FlightBooking = () => {
   const [adultCount, setAdultCount] = useState(1);
+  const [statusMessage, setStatusMessage] = useState("");
 
   const incrementCount = () => {
+    if (adultCount === MAX_PASSENGERS) {
+      setStatusMessage(
+        `최대 승객 수에 도달했습니다. 현재 승객 수는 ${adultCount}명 입니다.`
+      );
+      return;
+    }
     setAdultCount((prev) => Math.min(MAX_PASSENGERS, prev + 1));
   };
 
   const decrementCount = () => {
+    if (adultCount === MIN_PASSENGERS) {
+      setStatusMessage(
+        `최소 승객 수에 도달했습니다. 현재 승객 수는 ${adultCount}명 입니다.`
+      );
+      return;
+    }
     setAdultCount((prev) => Math.max(1, prev - 1));
   };
 
@@ -38,6 +52,11 @@ const FlightBooking = () => {
           >
             +
           </button>
+          {statusMessage && (
+            <div className="visually-hidden" role="alert">
+              {statusMessage}
+            </div>
+          )}
         </div>
       </div>
       <button className="search-button">항공편 검색</button>
